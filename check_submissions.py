9ac97be8fa1bdffdce8f62bed6330fdacc3da2d6
Ayush Dubey <33064931+devAyushDubey@users.noreply.github.com>
2022-09-29 23:19:04 +0530

    Added Logo in script

--- check_submissions.py
+++ check_submissions.py
@@ -75,7 +75,7 @@ def check_structure(path, student: Student):
 
 def write_to_readme(filename, students_list):
 
-    mdFile = MdUtils(file_name=filename, title="Welcome to Coderspree2.0 🔥")
+    mdFile = MdUtils(file_name=filename, title="Welcome to Coderspree2.0 🔥 ![Innogeeks Logo](https://user-images.githubusercontent.com/33064931/193105033-d03aa2e9-d4c3-4d02-b33e-466f3c567f39.png)")
 
     mdFile.new_paragraph(
         mdFile.new_inline_image(
