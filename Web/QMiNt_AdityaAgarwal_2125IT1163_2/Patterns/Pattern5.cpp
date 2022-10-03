6b3aa5dfd97569eb1b9b0ca50cb5a4b499789210
Aditya Agarwal <agarwaladitya27257@outlook.com>
2022-10-03 20:51:53 +0530

    started Pattern

---
+++ Web/QMiNt_AdityaAgarwal_2125IT1163_2/Patterns/Pattern5.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n;
+    cin>>n;
+    int t=(n+1/2);
+    for (int i = 0; i < n; i++)
+    {
+    
+        cout<<endl;
+    }
+    
+}
