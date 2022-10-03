cd6d9755979168cfd4c0db958814a928870c07cb
Tushar Sahani <tusharsahani000@gmail.com>
2022-10-03 20:14:13 +0530

    GettingStarted

---
+++ Web/Tushar-sahani_TusharSahani_2125cs1072_2/GettingStarted/benjaminbulbs.java
@@ -0,0 +1,15 @@
+package coderspear;
+
+import java.util.*;
+public class benjaminbulbs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1;i*i<=n;i++){
+            
+            System.out.println(i*i);
+            
+        }
+        sc.close();
+   }
+}
