9d5a306906b958595f316a75b8274a4e64ad065c
Divyanshu-Vareshney <varshneydivyanshu2904@gmail.com>
2022-10-05 00:14:56 +0530

    questions

---
+++ Web/Divyanshu-Vareshney_divyanshuvarshney_2125it1075_2/getting started/Reverse A Number.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+
+    //write your code here
+    int i;
+    while(n>0){
+        i=n%10;
+        cout<<i<<"\n";
+        n=n/10;
+    }
+    
+}
