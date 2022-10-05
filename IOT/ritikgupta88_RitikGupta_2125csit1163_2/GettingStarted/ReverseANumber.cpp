6ba94f60c22a2a791f19adb563aee1f8ad4522eb
ritikgupta88 <ritikgupta7452@gmail.com>
2022-10-05 18:40:27 +0530

    Completed 10 questions

---
+++ IOT/ritikgupta88_RitikGupta_2125csit1163_2/GettingStarted/ReverseANumber.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv){
+long long int n,d,t,rev=0,r;
+    cin >> n;
+    t=n;
+    while(n!=0){
+        d=n%10;
+        cout<<d<<endl;
+        n=n/10;
+ 
+ }
+ 
+}
