e8b6189f7a216f00bd4869077bfa2f22e662e718
rishu15 <hrishikdwivedi@gmail.com>
2022-10-08 15:54:44 +0530

    Update printDigits.cpp

--- Web/rishu15_HrishikDwivedi_2125ec1006_02/gettingStarted/printDigits.cpp
+++ Web/rishu15_HrishikDwivedi_2125ec1006_02/gettingStarted/printDigits.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 using namespace std;
-
+# define ll long long
 int main(int argc, char **argv){
-    int n;
+    ll n;
     cin >> n;
 
     //write your code here
-
-    int temp = n;
+  
+    ll temp = n;
     int d=0;
-    int sum=0;
+    ll sum=0;
     while(temp!=0){
-        int r=temp%10;
+       ll r=temp%10;
         temp/=10;
         sum=sum*10 + r;
     }
     while(sum!=0){
-        int r=sum%10;
+        ll r=sum%10;
         cout<<r<<endl;
         sum/=10;
     }
