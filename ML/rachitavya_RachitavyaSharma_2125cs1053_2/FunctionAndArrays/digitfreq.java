9973d91e3b347202b2bd0b22f8b89580b3188a35
Rachitavya Sharma <rachitavya@gmail.com>
2022-10-18 22:40:31 +0530

    new folder

---
+++ ML/rachitavya_RachitavyaSharma_2125cs1053_2/FunctionAndArrays/digitfreq.java
@@ -0,0 +1,5 @@
+package ML.rachitavya_RachitavyaSharma_2125cs1053_2.FunctionAndArrays;
+
+public class digitfreq {
+    
+}
---
+++ ML/rachitavya_RachitavyaSharma_2125cs1053_2/GettingStarted/bulbs.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class bulbs 
+{
+    public static void main(String args[])    
+    {
+        Scanner sc=new Scanner(System.in);
+        
+
+        sc.close();
+    }
+}
