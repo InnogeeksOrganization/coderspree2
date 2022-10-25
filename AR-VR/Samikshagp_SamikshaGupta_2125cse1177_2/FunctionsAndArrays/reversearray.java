48220baabacc227df62c6ac93ca2fb09d854a52d
Samikshagp <103514102+Samikshagp@users.noreply.github.com>
2022-10-25 18:51:22 +0530

    Functionsandarrays
    questions

---
+++ AR-VR/Samikshagp_SamikshaGupta_2125cse1177_2/FunctionsAndArrays/reversearray.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+  public static void display(int[] a){
+    StringBuilder sb = new StringBuilder();
+
+    for(int val: a){
+      sb.append(val + " ");
+    }
+    System.out.println(sb);
+  }
+
+  public static void reverse(int[] a){
+      for(int i=a.length-1;i>=0;i--)
+      {
+          System.our.print(a[i]);
+      }
+    // write your code here
+  }
+
+public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+    int[] a = new int[n];
+    for(int i = 0; i < n; i++){
+       a[i] = Integer.parseInt(br.readLine());
+    }
+
+    reverse(a);
+    display(a);
+ }
+
+}
